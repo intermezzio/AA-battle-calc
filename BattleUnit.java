2d3689b1b8bc5f505fa3344f959ce3215c4ca8d8
Andrew M <anmascillaro@ctemc.org>
2018-03-03 23:08:32 -0500

    Create BattleUnit Constructors
    Override and default constructors made

--- BattleUnit.java
+++ BattleUnit.java
@@ -1,5 +1,15 @@
 public class BattleUnit {
+	byte attackValue;
+	byte defendValue;
+
 	public BattleUnit() {
-		
+		this((byte)attackValue, (byte)defendValue);
+	}
+	public BattleUnit(int attackValue, int defendValue) {
+		this((byte)attackValue, (byte)defendValue);
+	}
+	public BattleUnit(byte attackValue, byte defendValue) {
+		this.attackValue = attackValue;
+		this.defendValue = defendValue;
 	}
 }
