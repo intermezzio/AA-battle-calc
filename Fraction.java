fd45c98fb8c2ac6b93c32427967f5632ffe49deb
Andrew M <anmascillaro@ctemc.org>
2018-03-10 22:23:26 -0500

    Create Fraction class

---
+++ Fraction.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+public class Fraction extends Number implements Comparable<Fraction> {
+	private BigInteger numerator;
+	private BigInteger denominator;
+
+	public Fraction(Number num, Number den) {
+		
+	}
+}
