ab7ef05c371c51eb219729e941ca95afd88c1552
Andrew M <anmascillaro@ctemc.org>
2018-03-19 20:20:57 -0400

    Change fraction constructor to integers

--- Fraction.java
+++ Fraction.java
@@ -3,7 +3,7 @@ public class Fraction extends Number implements Comparable<Fraction> {
 	private BigInteger numerator;
 	private BigInteger denominator;
 
-	public Fraction(Number num, Number den) {
+	public Fraction(int num, int den) {
 		
 	}
 }
