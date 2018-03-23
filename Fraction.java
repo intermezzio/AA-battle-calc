4979cd6171656839bdf6f3536bcbdbbd96fd1809
Andrew M <anmascillaro@ctemc.org>
2018-03-22 22:17:56 -0400

    Update Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -4,6 +4,7 @@ public class Fraction extends Number implements Comparable<Fraction> {
 	private BigInteger denominator;
 
 	public Fraction(int num, int den) {
-		
+		numerator = new BigInteger(num);
+		denominator = new BigInteger(den);
 	}
 }
