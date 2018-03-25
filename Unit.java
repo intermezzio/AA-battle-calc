01091aeb090e9804edc2d977f95ef9af2e2475a8
Andrew M <anmascillaro@ctemc.org>
2018-03-25 14:05:26 -0400

    Continue Previous Commit

---
+++ Unit.java
@@ -0,0 +1,20 @@
+public class Unit {
+	byte attackValue;
+	byte defendValue;
+	byte hitsLeft;
+
+	public BattleUnit() {
+		this((byte)attackValue, (byte)defendValue);
+	}
+	public BattleUnit(int attackValue, int defendValue) {
+		this((byte)attackValue, (byte)defendValue);
+	}
+	public BattleUnit(byte attackValue, byte defendValue) {
+		this(attackValue, defendValue, 1);
+	}
+	public BattleUnit(byte attackValue, byte defendValue, byte hitsLeft) {
+		this.attackValue = attackValue;
+		this.defendValue = defendValue;
+		this.hitsLeft = hitsLeft;
+	}
+}
