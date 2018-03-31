f7c50757ecddd3bfc48516924ceee13211f437d4
Andrew M <anmascillaro@ctemc.org>
2018-03-30 23:54:44 -0400

    BigInteger -> long
    I don't think I need really big numbers
    Also throws divide by 0 error

--- Fraction.java
+++ Fraction.java
@@ -1,7 +1,9 @@
 import java.math.BigInteger;
-public class Fraction extends Number implements Comparable<Fraction> {
-	private BigInteger numerator;
-	private BigInteger denominator;
+public class Fraction extends Number implements Comparable<Fraction> throws IllegalArgumentException {
+	private long numerator;
+	private long denominator;
+	public Fraction ZERO = new Fraction(0,1);
+	public Fraction ONE = new Fraction(1,1);
 
 	public Fraction(int num, int den) {
 		numerator = new BigInteger(num);
