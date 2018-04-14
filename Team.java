ed13222d977c7b82c4e3f50f20708c40b7951408
Andrew M <anmascillaro@ctemc.org>
2018-04-14 00:20:44 -0400

    Add private variables to Team

--- Team.java
+++ Team.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
 
 public class Team {
+	private ArrayList<Unit> units;
+	private ArrayList<Unit> casualties;
+	private byte [] hitsequence;
+
 	/*
 	Team includes hitsequence, etc.
 	*/
