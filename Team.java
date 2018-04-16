0e120ada30f91248115e88ec3e6511c9e0784fa1
Andrew M <anmascillaro@ctemc.org>
2018-04-15 23:28:48 -0400

    Team constructor

--- Team.java
+++ Team.java
@@ -8,4 +8,13 @@ public class Team {
 	/*
 	Team includes hitsequence, etc.
 	*/
-}
+	public Team (ArrayList<Team> units) {
+		this(units, new byte [units.size()]);
+	}
+	public Team (ArrayList<Team> units, byte [] hitsequence) {
+		this.units = units;
+		this.casualties = new ArrayList<Team>();
+		this.hitsequence = hitsequence;
+	}
+
+} 
